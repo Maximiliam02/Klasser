ddadbea36d5a1ae4396d1673fc7eb94728a9ae7d
Maximiliam02 <54435275+Maximiliam02@users.noreply.github.com>
2019-12-17 13:54:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Klasser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fordon f = new Fordon();
+            f.setTillverkare("Stefan");
+            f.setModell("volvo");
+            f.setÅrsmodell(2020);
+            f.setVikt(900);
+
+           
+        }
+
+    }
+}
---
+++ fordon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Klasser
+{
+    class Fordon
+    {
+        Fordon f = new Fordon();
+        private string tillverkare;
+        private string modell;
+        private int årsmodell;
+        private int vikt;
+
+        public void setTillverkare(string a) { tillverkare = a; }
+        public string getTillverkare() { return tillverkare; }
+
+        public void setModell(string b) { modell = b; }
+        public string getModell() { return modell; }
+
+        public void setÅrsmodell(int c) { årsmodell = c; }
+        public int getÅrsmodell() { return årsmodell; }
+
+        public void setVikt(int d) { vikt = d; }
+        public int getVikt() { return vikt; }
+
+
+
+    }
+}
+
+
+
+
+
+}
